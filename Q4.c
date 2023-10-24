ffe7d97f8ff3d6b4f119a9fcdd82ff99f1b6f37d
ayankhan2021 <148139254+ayankhan2021@users.noreply.github.com>
2023-10-24 23:14:29 +0500

    Add files via upload

---
+++ Q4.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+void decimalToBinary(int decimal) {
+    if (decimal > 0) {
+        decimalToBinary(decimal / 2);
+        printf("%d", decimal % 2);
+    }
+}
+
+int main() {
+    int decimalNumber;
+
+    printf("Enter a decimal number: ");
+    scanf("%d", &decimalNumber);
+
+    if (decimalNumber == 0) {
+        printf("Binary representation: 0\n");
+    } else {
+        printf("Binary representation: ");
+        decimalToBinary(decimalNumber);
+    }
+
+    return 0;
+}
+#include <stdio.h>
+
+void decimalToBinary(int decimal) {
+    if (decimal > 0) {
+        decimalToBinary(decimal / 2);
+        printf("%d", decimal % 2);
+    }
+}
+
+int main() {
+    int decimalNumber;
+
+    printf("Enter a decimal number: ");
+    scanf("%d", &decimalNumber);
+
+    if (decimalNumber == 0) {
+        printf("Binary representation: 0\n");
+    } else {
+        printf("Binary representation: ");
+        decimalToBinary(decimalNumber);
+    }
+
+    return 0;
+}
